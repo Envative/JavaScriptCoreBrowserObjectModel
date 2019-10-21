a56d0a0f13b3531725cbf8dc1946381d4cc29b87
Garrett Shearer <garrett.shearer@gmail.com>
2019-10-21 12:06:40 -0400

    fixed timers, not firing because they were being created on background threads

--- Source/Timers.swift
+++ Source/Timers.swift
@@ -81,45 +81,56 @@ import JavaScriptCore
     internal class func createTimer(callback: JSValue, delay: Double, repeats: Bool) -> UInt {
         let callbackManagedValue = JSManagedValue(value: callback)!
         let timerId = prevTimerId + 1
-        prevTimerId = timerId
-        let timer = Timer.scheduledTimer(
-            timeInterval: delay/1000.0,
-            target: self,
-            selector: #selector(fireTimer(timer:)),
-            userInfo: TimerData(id: timerId, callbackManagedValue: callbackManagedValue, repeats: repeats),
-            repeats: repeats
-        )
-        /*
-        // NOTE: the following implementation requires iOS 10, so we'll switch to it eventually
-        let timer = Timer.scheduledTimer(withTimeInterval: delay/1000.0, repeats: repeats) { timer in
-            let callback = callbackManagedValue.value
-            guard callback?.call(withArguments: []) != nil else {
-                // INFO: callback no-longer exists, or is not a function
-                timer.invalidate()
-                timers.removeValue(forKey: timerId)
-                return
+        
+        DispatchQueue.main.async {
+            self.prevTimerId = timerId
+            let timer = Timer.scheduledTimer(
+                timeInterval: delay/1000.0,
+                target: self,
+                selector: #selector(fireTimer(timer:)),
+                userInfo: TimerData(id: timerId, callbackManagedValue: callbackManagedValue, repeats: repeats),
+                repeats: repeats
+            )
+            /*
+            // NOTE: the following implementation requires iOS 10, so we'll switch to it eventually
+            let timer = Timer.scheduledTimer(withTimeInterval: delay/1000.0, repeats: repeats) { timer in
+                let callback = callbackManagedValue.value
+                guard callback?.call(withArguments: []) != nil else {
+                    // INFO: callback no-longer exists, or is not a function
+                    timer.invalidate()
+                    timers.removeValue(forKey: timerId)
+                    return
+                }
+                if !repeats {
+                    timers.removeValue(forKey: timerId)
+                }
             }
-            if !repeats {
-                timers.removeValue(forKey: timerId)
-            }
+            */
+            
+            callback.context.virtualMachine.addManagedReference(callbackManagedValue, withOwner: timer)
+            self.timers[timerId] = timer
         }
-        */
-        callback.context.virtualMachine.addManagedReference(callbackManagedValue, withOwner: timer)
-        timers[timerId] = timer
+        //RunLoop.current.add(timer, forMode: .defaultRunLoopMode)
         return timerId
     }
     
     @objc internal class func fireTimer(timer: Timer) {
         let userInfo = timer.userInfo as! TimerData
-        let callback = userInfo.callbackManagedValue.value
-        guard callback?.call(withArguments: []) != nil else {
-            // INFO: callback no-longer exists, or is not a function
-            timer.invalidate()
-            timers.removeValue(forKey: userInfo.id)
-            return
-        }
-        if !userInfo.repeats {
-            timers.removeValue(forKey: userInfo.id)
+    
+        if let callback = userInfo.callbackManagedValue.value {
+            if callback.isNull || callback.isUndefined {
+                timer.invalidate()
+                timers.removeValue(forKey: userInfo.id)
+            }
+            guard callback.call(withArguments: []) != nil else {
+                // INFO: callback no-longer exists, or is not a function
+                timer.invalidate()
+                timers.removeValue(forKey: userInfo.id)
+                return
+            }
+            if !userInfo.repeats {
+                timers.removeValue(forKey: userInfo.id)
+            }
         }
     }
     
